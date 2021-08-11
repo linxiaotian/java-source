d319b65408922ddf8e8b2e13e799cee103265aeb
林强 <19817153610@139.com>
2021-08-11 16:17:10 +0800

    init

---
+++ src/main/java/com/lxt/learnsource/LearnSourceApplication.java
@@ -0,0 +1,13 @@
+package com.lxt.learnsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lxt/learnsource/LearnSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxt.learnsource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
